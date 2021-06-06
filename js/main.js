17a09d3f4a68627c2c0df9d5644ad02d397e20c7
Amal <61520524+AmalAljabri@users.noreply.github.com>
2021-06-06 11:07:54 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,1 +1,29 @@
+var btntop = document.getElementById("btntop");
 
+window.onscroll = function() { scrollFunction() };
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        btntop.style.display = "block";
+    } else {
+        btntop.style.display = "none";
+    }
+}
+
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+
+
+const countEl = document.getElementById('count');
+
+updateVisitCount();
+
+function updateVisitCount() {
+    fetch('https://api.countapi.xyz/hit/amalaljabri.github.io/visits')
+        .then(res => res.json())
+        .then(res => {
+            countEl.innerHTML = res.value;
+        })
+}
